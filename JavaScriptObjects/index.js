ec2e79b4bcd837a6c330823e70805297ff39ad6d
zmarcosb <46399568+zmarcosb@users.noreply.github.com>
2019-01-24 14:49:53 -0500

    Add files via upload

---
+++ JavaScriptObjects/index.js
@@ -0,0 +1,26 @@
+
+// var name = "Vanessa";
+// var lastName = "Marcos";
+// var age = 25;
+// var sport = "Karate";
+
+// document.write(name);
+
+
+// Create an object: 
+let person1  = {
+firstName: "Vanessa", 
+lastName: "Marcos", 
+age: 25,
+sport: "Karate" 
+}; 
+//document.write(person.firstName);
+document.write(person1.firstName + " likes " + person1.sport + "<br>");
+
+let person2  = {
+firstName: "Joseph", 
+lastName: "Baker", 
+age: 18,
+sport: "Karate" 
+}; 
+document.write(person2.firstName + " is " + person2.age + " years old.");
