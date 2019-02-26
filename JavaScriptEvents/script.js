8e6a4f31317668d4b63cf3a610eff5b796642563
zmarcosb <46399568+zmarcosb@users.noreply.github.com>
2019-02-26 14:05:01 -0500

    Add files via upload

---
+++ JavaScriptEvents/script.js
@@ -0,0 +1,15 @@
+var numOne = document.getElementById("n1");
+var numTwo = document.getElementById("n2");
+var addSum = document.getElementById("sum");
+
+numOne.addEventListener("input", add);
+numTwo.addEventListener("input", add);
+
+function add(){
+    var one = parseFloat(numOne.value) || 0;
+    var two = parseFloat(numTwo.value) || 0;
+    var result = one + two;
+    addSum.innerHTML = "Result: " + result;
+}
+
+/**********************************************************/
