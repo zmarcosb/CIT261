7d3653b277d17c367034449a25018518097204d3
zmarcosb <46399568+zmarcosb@users.noreply.github.com>
2019-02-27 10:18:25 -0500

    Update script.js

--- Json/script.js
+++ Json/script.js
@@ -2,16 +2,14 @@ var Jair = new Object();
 Jair.height = 1.70;
 Jair.age = 26;
 Jair.eyesColor = "green";
+function displayString() {
+document.getElementById('showString').innerHTML = "The characteristics of Jair are: " + JSON.stringify(Jair);
+}
 
-document.write ("The characteristics of Jair are: " + JSON.stringify(Jair));
+var info = '{"name": "Rose","lastName": "Smith","age": 25,"hobbie": "sing"}';
+    
+var parseInfo = JSON.parse(info);
 
-
-function jsonParse(){ //Creating a function called when the page loads
-    var inString = prompt("Type a JSON object");
-    var output = "";
-    myObject = JSON.parse(inString);
-    for(i in myObject){
-        output += "Characteristic: " + i + " = " + myObject[i] + '\n';
-    }
-    alert(output);
+function displayParse() {
+document.getElementById('txt').innerHTML = "Name:      " + parseInfo.name + "<br>" + "Last Name: " + parseInfo.lastName + "<br>" + "Age: " + parseInfo.age + "<br>" + "Hobbie: " + parseInfo.hobbie;
 }
